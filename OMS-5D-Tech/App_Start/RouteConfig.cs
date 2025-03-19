c24685d945ddd38b692c1254aff26d75fb288631
Nguyen Quang Duy <nguyenquangduy12112004@gmail.com>
2025-03-19 11:16:06 +0700

    xong 1 controller

--- OMS-5D-Tech/App_Start/RouteConfig.cs
+++ OMS-5D-Tech/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace OMS_5D_Tech
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",

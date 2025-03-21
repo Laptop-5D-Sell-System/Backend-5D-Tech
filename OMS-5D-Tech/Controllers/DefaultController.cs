15c2fdd82691113108628c75448f491fed24f383
Nguyễn Tiến Dũng <120901051+Bekend10@users.noreply.github.com>
2025-03-21 16:47:14 +0700

    update account controller

---
+++ OMS-5D-Tech/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OMS_5D_Tech.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Content("API 5D-Tech is running");
+        }
+    }
+}

3fd3ab845ea13210e4b70c59713c6652de974b5f
Bekend10 <tiendungk18a1@gmail.com>
2025-03-22 14:42:00 +0700

    Update account

--- OMS-5D-Tech/App_Start/Startup.cs
+++ OMS-5D-Tech/App_Start/Startup.cs
@@ -4,6 +4,12 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.AspNet.Identity;
 using System.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.Jwt;
+using System.Text;
+using System;
+using Microsoft.Owin.Security;
+using OMS_5D_Tech.Middlewares;
 
 [assembly: OwinStartup(typeof(OMS_5D_Tech.Startup))]
 
@@ -13,6 +19,7 @@ namespace OMS_5D_Tech
     {
         public void Configuration(IAppBuilder app)
         {
+            var key = Encoding.UTF8.GetBytes("NBTrvShWqPMJCjwKht3gucS7gTM4TY1vsBXW6dM588ViBMwqLnlsX5nFTf67jS_vTaDzidy6HlUza4rmBb67Lg==");
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
@@ -27,6 +34,23 @@ namespace OMS_5D_Tech
                 Scope = { "email", "profile", "openid" },
                 SignInAsAuthenticationType = DefaultAuthenticationTypes.ApplicationCookie 
             });
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }
+            });
+
+            var jwtService = new JwtService();
+            app.Use<JwtMiddleware>(jwtService);
         }
     }
 }
